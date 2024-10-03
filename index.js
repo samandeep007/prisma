e767eb939aad36134d5ac1ac30844684b5be6d60
samandeep007 <hacktechnicals@gmail.com>
2024-10-02 19:33:32 -0600

    configure express

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import 'dotenv/config'
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+//Regular middlewares
+app.use(express.urlencoded({ 
+    limit: "16kb",
+    extended: true
+}));
+
+app.use(express.json({ //From the request body, we will be able to use json using this
+    limit: "16kb"
+}))
+
+//Cookie middleware --> To trap and verify the cookies
+app.use(cookieParser());
+
+app.get('/', (_, res) => {
+    res.send("Hello World");
+})
+
+app.listen(3000, () => {
+    console.log("The server is listening on port 3000");
+})
